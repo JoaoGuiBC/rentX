a9d0ffbabfecbdcc03a4a0f6f0dc7cca75c82ac6
JoaoGuiBC <joaoguibc@gmail.com>
2021-11-05 20:13:51 -0300

    New component: Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 
 import { Header } from '../../components/Header';
+import { Car } from '../../components/Car';
 
 import { Container } from './styles';
 
@@ -14,6 +15,25 @@ export function Home() {
         translucent
       />
       <Header />
+
+      <Car
+        brand="AUDI"
+        name="RS 5 Coupé"
+        rent={{
+          period: 'Ao dia',
+          price: 120,
+        }}
+        thumbnail="https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png"
+      />
+      <Car
+        brand="porsche"
+        name="Panamera"
+        rent={{
+          period: 'Ao dia',
+          price: 340,
+        }}
+        thumbnail="https://www.webmotors.com.br/imagens/prod/347515/PORSCHE_PANAMERA_4.0_V8_EHYBRID_TURBO_S_PDK_34751516170869484.png?s=fill&w=130&h=97&q=70&t=true"
+      />
     </Container>
   );
 }
