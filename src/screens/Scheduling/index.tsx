e3c53c7de35961b6f7240c58aadfddd4c4282025
JoaoGuiBC <joaoguibc@gmail.com>
2021-11-08 21:53:53 -0300

    Started the Scheduling screen

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+
+import { BackButton } from '../../components/BackButton';
+import { Button } from '../../components/Button';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+
+export function Scheduling() {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <BackButton color={theme.colors.shape} onPress={() => {}} />
+
+        <Title>
+          Escolha uma {'\n'}
+          data de início e {'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue isSelected>08 Novembro 2021</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue isSelected={false} />
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content />
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
